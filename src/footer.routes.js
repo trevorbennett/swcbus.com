d045779cbebf5d3b3f96e4fb1295e5fef6520946
Bennett, Trevor D <bennett.1430@osu.edu>
2023-08-25 12:13:41 -0400

    remove footer

--- src/footer.routes.js
+++ src/footer.routes.js
@@ -14,7 +14,7 @@ const date = new Date().getFullYear();
 
 export default {
   brand: {
-    name: "Material Kit 2",
+    name: "Startup Weekend Columbus",
     image: logoCT,
     route: "/",
   },
@@ -37,40 +37,40 @@ export default {
     },
   ],
   menus: [
-    {
-      name: "company",
-      items: [
-        { name: "about us", href: "https://www.creative-tim.com/presentation" },
-        { name: "freebies", href: "https://www.creative-tim.com/templates/free" },
-        { name: "premium tools", href: "https://www.creative-tim.com/templates/premium" },
-        { name: "blog", href: "https://www.creative-tim.com/blog" },
-      ],
-    },
-    {
-      name: "resources",
-      items: [
-        { name: "illustrations", href: "https://iradesign.io/" },
-        { name: "bits & snippets", href: "https://www.creative-tim.com/bits" },
-        { name: "affiliate program", href: "https://www.creative-tim.com/affiliates/new" },
-      ],
-    },
-    {
-      name: "help & support",
-      items: [
-        { name: "contact us", href: "https://www.creative-tim.com/contact-us" },
-        { name: "knowledge center", href: "https://www.creative-tim.com/knowledge-center" },
-        { name: "custom development", href: "https://services.creative-tim.com/" },
-        { name: "sponsorships", href: "https://www.creative-tim.com/sponsorships" },
-      ],
-    },
-    {
-      name: "legal",
-      items: [
-        { name: "terms & conditions", href: "https://www.creative-tim.com/terms" },
-        { name: "privacy policy", href: "https://www.creative-tim.com/privacy" },
-        { name: "licenses (EULA)", href: "https://www.creative-tim.com/license" },
-      ],
-    },
+    // {
+    //   name: "company",
+    //   items: [
+    //     { name: "about us", href: "https://www.creative-tim.com/presentation" },
+    //     { name: "freebies", href: "https://www.creative-tim.com/templates/free" },
+    //     { name: "premium tools", href: "https://www.creative-tim.com/templates/premium" },
+    //     { name: "blog", href: "https://www.creative-tim.com/blog" },
+    //   ],
+    // },
+    // {
+    //   name: "resources",
+    //   items: [
+    //     { name: "illustrations", href: "https://iradesign.io/" },
+    //     { name: "bits & snippets", href: "https://www.creative-tim.com/bits" },
+    //     { name: "affiliate program", href: "https://www.creative-tim.com/affiliates/new" },
+    //   ],
+    // },
+    // {
+    //   name: "help & support",
+    //   items: [
+    //     { name: "contact us", href: "https://www.creative-tim.com/contact-us" },
+    //     { name: "knowledge center", href: "https://www.creative-tim.com/knowledge-center" },
+    //     { name: "custom development", href: "https://services.creative-tim.com/" },
+    //     { name: "sponsorships", href: "https://www.creative-tim.com/sponsorships" },
+    //   ],
+    // },
+    // {
+    //   name: "legal",
+    //   items: [
+    //     { name: "terms & conditions", href: "https://www.creative-tim.com/terms" },
+    //     { name: "privacy policy", href: "https://www.creative-tim.com/privacy" },
+    //     { name: "licenses (EULA)", href: "https://www.creative-tim.com/license" },
+    //   ],
+    // },
   ],
   copyright: (
     <MKTypography variant="button" fontWeight="regular">
